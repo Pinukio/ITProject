c77d85e110d34fac275d881659963433d1bd4eec
Kartarss <jw5150@naver.com>
2019-11-22 23:40:05 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/example/itproject/ImageActivity.kt

--- app/src/main/java/com/example/itproject/MakeSetActivity.kt
+++ app/src/main/java/com/example/itproject/MakeSetActivity.kt
@@ -1,11 +1,16 @@
 package com.example.itproject
 
+import android.content.Context
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_make_set.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -17,6 +22,9 @@ class MakeSetActivity : AppCompatActivity() {
     private var list : MutableList<Model>? = null
     private var array_meaning : ArrayList<String>? = null
     private var adapter : MakeSetAdapter? = null
+    private var dialog : AlertDialog? = null
+    private var array_null : ArrayList<Int>? = null // 뜻이 리턴되지 않은 단어들의 인덱스 저장
+    //private var itemTouchHelper : ItemTouchHelper? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,8 +33,18 @@ class MakeSetActivity : AppCompatActivity() {
         val intent = intent
         array_word = intent.getStringArrayListExtra("array_word")
         array_meaning = ArrayList()
+        array_null = ArrayList()
+
 
         if(array_word != null) {
+
+            val builder : AlertDialog.Builder = AlertDialog.Builder(this)
+            val inflater : LayoutInflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            builder.setView(inflater.inflate(R.layout.dialog_loading, null))
+            builder.setCancelable(false)
+            dialog = builder.create()
+            dialog!!.show()
+
             for(i in 0 until array_word!!.size)
                 array_meaning!!.add("")
 
@@ -36,8 +54,14 @@ class MakeSetActivity : AppCompatActivity() {
                     override fun onFailure(call: Call<WordRepo>, t: Throwable) {}
                     override fun onResponse(call: Call<WordRepo>, response: Response<WordRepo>) {
                         val wordrepo: WordRepo? = response.body()
-                        array_meaning!![index] = wordrepo?.meaning?.korean!!
+                        if (wordrepo?.meaning?.korean == null) {
+                            array_meaning!![index] = ""
+                            array_null!!.add(index)
+                        }
+                        else
+                        array_meaning!![index] = wordrepo.meaning.korean
                         MakeListTask().execute()
+
                     }
                 })
             }
@@ -50,12 +74,18 @@ class MakeSetActivity : AppCompatActivity() {
                 add(Model(Model.CARD_TYPE, "", ""))
             }
 
-            val adapter = MakeSetAdapter(list!!)
+            adapter = MakeSetAdapter(list!!)
             MakeSet_recycler.adapter = adapter
             MakeSet_recycler.layoutManager = LinearLayoutManager(this)
+            //itemTouchHelper = ItemTouchHelper(simpleCallback)
+            //itemTouchHelper!!.attachToRecyclerView(MakeSet_recycler)
         }
 
-        MakeSet_check.setOnClickListener {
+        MakeSet_trash.setOnClickListener {
+            adapter!!.deleteItems()
+        }
+
+        MakeSet_checkbtn.setOnClickListener {
             if(adapter != null) {
                 val title : String = MakeSet_recycler.findViewHolderForAdapterPosition(0)!!.itemView.findViewById<EditText>(R.id.MakeSet_title).text.toString()
                 val subtitle : String = MakeSet_recycler.findViewHolderForAdapterPosition(1)!!.itemView.findViewById<EditText>(R.id.MakeSet_subtitle).text.toString()
@@ -65,13 +95,13 @@ class MakeSetActivity : AppCompatActivity() {
 
     inner class MakeListTask : AsyncTask<Void, Void, MutableList<Model>>() {
 
+
         override fun doInBackground(vararg params: Void?): MutableList<Model> {
 
             list = mutableListOf<Model>().apply {
                 add(Model(Model.TITLE_TYPE, "", ""))
                 for(i in 0 until array_meaning!!.size) {
                     add(Model(Model.CARD_TYPE, array_word!![i], array_meaning!![i]))
-                    Log.i(array_word!![i], array_meaning!![i])
                 }
             }
             return list!!
@@ -84,6 +114,72 @@ class MakeSetActivity : AppCompatActivity() {
             adapter = MakeSetAdapter(result)
             MakeSet_recycler.adapter = adapter
             MakeSet_recycler.layoutManager = LinearLayoutManager(applicationContext)
+            //itemTouchHelper = ItemTouchHelper(simpleCallback)
+            //itemTouchHelper!!.attachToRecyclerView(MakeSet_recycler)
+
+            var tmp = false
+
+            for(i in 0 until array_meaning!!.size) {
+                if(array_meaning!![i] == "") {
+
+                    for(j in 0 until array_null!!.size)
+                        if(i == array_null!![j])
+                            tmp = true
+                }
+                else
+                    tmp = true
+            }
+
+            if(tmp)
+                dialog!!.dismiss()
         }
     }
+
+    fun updateList() {
+        list = mutableListOf<Model>().apply {
+            add(Model(Model.TITLE_TYPE, "", ""))
+            for(i in 0 until array_meaning!!.size) {
+                add(Model(Model.CARD_TYPE, array_word!![i], array_meaning!![i]))
+            }
+        }
+
+        adapter = MakeSetAdapter(list!!)
+    }
+
+    /*val simpleCallback : ItemTouchHelper.SimpleCallback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ): Boolean {
+            return false
+        }
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            val position = viewHolder.adapterPosition
+            //타이틀 부분을 제외해야 함
+            if(array_word != null) {
+
+                if(array_word!!.size != 0) {
+                    *//*val index = position - 1
+                    array_word!!.removeAt(index)
+                    array_meaning!!.removeAt(index)
+
+                    for(i in 0..index)
+                        if(array_null!![i] == index)
+                            array_null!!.remove(index)*//*
+
+                    adapter!!.removeItem(position)
+                }
+
+            }
+
+            else {
+                adapter!!.removeItem(position)
+            }
+
+        }
+
+    }*/
+
 }
--- app/src/main/java/com/example/itproject/MakeSetAdapter.kt
+++ app/src/main/java/com/example/itproject/MakeSetAdapter.kt
@@ -1,23 +1,28 @@
 package com.example.itproject
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
 import android.widget.EditText
 import androidx.recyclerview.widget.RecyclerView
-import java.lang.RuntimeException
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MakeSetAdapter(val list : MutableList<Model>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-
-    /*private var array_word : ArrayList<String>? = null
-    private var array_meaning : ArrayList<String>? = null
-    private lateinit var title : String
-    private lateinit var subtitle : String*/
+    var array_checkbox : ArrayList<CheckBox>? = null
+    var array_selected : ArrayList<Int>? = null
+    var array_isChecked : ArrayList<Boolean>? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val view : View?
-        /*array_word = ArrayList()
-        array_meaning = ArrayList()*/
+        array_checkbox = ArrayList()
+        array_selected = ArrayList()
+        array_isChecked = ArrayList()
+        for(i in 0 until list.size - 1)
+            array_isChecked!!.add(false)
+
         return when(viewType) {
 
             Model.TITLE_TYPE -> {
@@ -27,6 +32,7 @@ class MakeSetAdapter(val list : MutableList<Model>) : RecyclerView.Adapter<Recyc
 
             Model.CARD_TYPE -> {
                 view = LayoutInflater.from(parent.context).inflate(R.layout.card_make_set, parent, false)
+                Log.i("Hello", "Hello")
                 CardTypeViewHolder(view)
             }
 
@@ -46,14 +52,28 @@ class MakeSetAdapter(val list : MutableList<Model>) : RecyclerView.Adapter<Recyc
             }
 
             Model.CARD_TYPE -> {
+
                 (holder as CardTypeViewHolder).et_word.setText(item.text)
                 holder.et_meaning.setText(item.text2)
-                /*array_word!!.add(item.text)
-                array_meaning!!.add(item.text2)*/
+                holder.checkbox.tag = position
+
+                holder.checkbox.setOnClickListener {
+                    val tmp = it.tag as Int
+                    if(!array_isChecked!![tmp - 1]) {
+                        array_selected!!.add(tmp)
+                        array_isChecked!![tmp - 1] = true
+                        Log.i("Hello", tmp.toString())
+                    }
+
+                    else {
+                        array_selected!!.remove(tmp)
+                        array_isChecked!![tmp - 1] = false
+                    }
+
+                }
             }
         }
     }
-
 
     override fun getItemCount(): Int {
         return list.size
@@ -64,20 +84,38 @@ class MakeSetAdapter(val list : MutableList<Model>) : RecyclerView.Adapter<Recyc
     }
 
     inner class TitleTypeViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
-        var et_title : EditText = itemView.findViewById(R.id.MakeSet_title)
+        val et_title : EditText = itemView.findViewById(R.id.MakeSet_title)
         val et_subtitle : EditText = itemView.findViewById(R.id.MakeSet_subtitle)
     }
 
     inner class CardTypeViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
         val et_word : EditText = itemView.findViewById(R.id.MakeSet_word)
         val et_meaning : EditText = itemView.findViewById(R.id.MakeSet_meaning)
+        val checkbox : CheckBox = itemView.findViewById(R.id.MakeSet_checkbox)
+
     }
 
-    /*fun getWords() : ArrayList<String> {
-        return array_word!!
+    private fun removeItem(position : Int) {
+        list.removeAt(position)
+        //array_selected!!.remove(position)
+        //array_checkbox!!.removeAt(position - 1)
+        notifyItemRemoved(position)
     }
 
-    fun getMeanings() : ArrayList<String> {
-        return array_meaning!!
-    }*/
+    fun deleteItems() {
+        Collections.sort(array_selected!!, AscendingInteger())
+        val iter : MutableIterator<Int> = array_selected!!.iterator()
+        while(iter.hasNext()) {
+            removeItem(iter.next())
+            iter.remove()
+            Log.i("궁금", array_selected!!.size.toString())
+        }
+    }
+
+    internal inner class AscendingInteger : Comparator<Int> {
+        override fun compare(a: Int, b: Int): Int {
+            return b.compareTo(a)
+        }
+    }
+
 }
